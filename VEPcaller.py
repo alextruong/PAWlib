962ff2118cc290e099d27590a43e15095cd5976f
joeyorofino <joeyorofino@users.noreply.github.com>
2014-09-28 20:03:40 -0400

    Update VEPcaller.py

--- VEPcaller.py
+++ VEPcaller.py
@@ -8,6 +8,7 @@ import glob
 
 
 def find_vep():
+        #locates the VEP executable
         print 'Locating VEP installation...'
         
         find_veploc = commands.getstatusoutput('find ~/ -name variant_effect_predictor.pl -print 2>/dev/null')
@@ -21,6 +22,7 @@ def find_vep():
 
 def vepcaller(filename, veploc):
         
+        #the port 3377 is for hg19
         print 'Running VEP on %s...' % filename
         noextension = filename[0:-4]
 
@@ -37,10 +39,12 @@ def main():
 
         veploc = find_vep()
 
+        #grabs all of the monoallelic potential variant files, and potential rna-editing files
         monoallelic_files = glob.glob('*_RNA-hom_WES-het.vcf')
         RNA_editing_files = glob.glob('*_RNA-het_WES-hom.vcf')
         full_merge = glob.glob('*_merged.vcf')
 
+        #annotates each file in the lists above
         for monoallelic in monoallelic_files:
                         vepcaller(monoallelic, veploc)
                         
